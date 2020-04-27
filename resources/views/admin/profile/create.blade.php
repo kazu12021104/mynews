fad8a20144b73d1f2cad7b29246c817c5ee5801f
EC2 Default User <ec2-user@ip-172-31-28-132.us-east-2.compute.internal>
2020-04-27 06:39:27 +0000

    PHP/Laravel13_task

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -12,18 +12,54 @@
     </body>
 </html>
 
-{{-- layouts/profile.blade.phpを読み込む --}}
 @extends('layouts.profile')
-
-{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
 @section('title', 'マイプロフィール')
 
-{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>プロフィール新規作成</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-date">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <lavel class="col-md-2">氏名</lavel>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <lavel class="col-md-2">性別</lavel>
+                        <div class="col-md-10">
+                            <select name="job">
+                                <option value="">選択してください</option>
+                                <option value="men">男性</option>
+                                <option value="women">女性</option>
+                                <option value="they">その他</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <lavel class="col-md-2">趣味</lavel>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="10">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
