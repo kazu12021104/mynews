cbc19eb7e5b4aff9d888d5b7ff420aa74ae64703
EC2 Default User <ec2-user@ip-172-31-28-132.us-east-2.compute.internal>
2020-05-06 08:12:07 +0000

    color_change

--- database/migrations/2020_05_04_154333_create_profilehistories_table.php
+++ database/migrations/2020_05_04_154333_create_profilehistories_table.php
@@ -15,7 +15,7 @@ class CreateProfilehistoriesTable extends Migration
     {
         Schema::create('profilehistories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('profile_id');
+            $table->integer('plofiles_id');
             $table->string('edited_at');
             $table->timestamps();
         });
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <hr color="#c0c0c0">
+        <hr color="#BDBDBD">
         @if (!is_null($headline))
             <div class="row">
                 <div class="headline col-md-10 mx-auto">
